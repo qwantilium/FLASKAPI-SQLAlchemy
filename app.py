f8089936928999690b71577b51b53fae6dc31b8b
unknown <qwantilium>
2022-10-13 00:45:45 +0500

    CRUD, API, Model

--- app.py
+++ app.py
@@ -1,38 +1,87 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 api = Api(app)
 
+# Делаем базу
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+db = SQLAlchemy(app)
+
+# Делаем модель
 
 
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(200), nullable=False)
+    data = db.Column(db.Integer(4), nullable=False)
+    publisher = db.Column(db.String(200), nullable=False)
+    isbn = db.Column(db.Integer(13), nullable=False)
 
-fields = {
+    def __repr__(self):
+        return self.title
+
+# Настраиваем сериализацию
+
+
+Bookfields = {
+    'id': fields.Integer,
     'title': fields.String(required=True, unique=True, nullable=False),
-    'author': fields.String(required=True,nullable=False),
+    'author': fields.String(required=True, nullable=False),
     'date': fields.Positive(required=True, nullable=False),
     'publisher': fields.String,
-
+    'isbn': fields.Integer,
 }
 
-class Books(Resource):
+# Настройки CRUD по REST
+
+
+class Bookitems(Resource):
+    @marshal_with(Bookfields)
     def get(self):
-        return {'hello': 'world'}
+        books = Book.query.all()
+        return books
 
-    def
-
-class Book(Resource):
-    def get(self):
-        return
-
+    @marshal_with(Bookfields)
     def post(self):
-        return
+        data = request.json
+        book = Book(title=data['title'])
+        db.session.add(book)
+        db.session.commit()
+        books = Book.query.all()
+        return books
 
 
+class Bookitem(Resource):
+    @marshal_with(Bookfields)
+    def get(self, id):
+        book = Book.query.filter_by(id=id).first()
+        return book
 
-api.add_resource(Books, '/')
-api.add_resource(Book, '/<int:id>')
+    @marshal_with(Bookfields)
+    def put(self, id):
+        data = request.json
+        book = Book.query.filter_by(id=id).first()
+        book.name = data['name']
+        db.session.commit()
+        return book
+
+    @marshal_with(Bookfields)
+    def delete(self, id):
+        book = Book.query.filter_by(id=id).first()
+        db.session.delete(book)
+        db.session.commit()
+        books = Book.query.all()
+        return books
+
+
+# Настройка маршрутов
+
+api.add_resource(Bookitems, '/')
+api.add_resource(Bookitem, '/<int:id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
