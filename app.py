cce944a00168ae460cda9efe15ff09a277c5311b
unknown <qwantilium>
2022-10-14 18:50:43 +0500

    fullrestapisqlachemy

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api, fields, marshal_with
+from flask_restful import Resource, Api, marshal_with, fields
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -13,25 +13,30 @@ db = SQLAlchemy(app)
 # Делаем модель
 
 
-class Book(db.Model):
+class Books(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     author = db.Column(db.String(200), nullable=False)
-    data = db.Column(db.Integer(4), nullable=False)
+    date = db.Column(db.Integer, nullable=False)
     publisher = db.Column(db.String(200), nullable=False)
-    isbn = db.Column(db.Integer(13), nullable=False)
+    isbn = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return self.title
 
+# Контекст создания базы
+
+
+with app.app_context():
+    db.create_all()
 # Настраиваем сериализацию
 
 
 Bookfields = {
     'id': fields.Integer,
-    'title': fields.String(required=True, unique=True, nullable=False),
-    'author': fields.String(required=True, nullable=False),
-    'date': fields.Positive(required=True, nullable=False),
+    'title': fields.String,
+    'author': fields.String,
+    'date': fields.Integer,
     'publisher': fields.String,
     'isbn': fields.Integer,
 }
@@ -42,39 +47,44 @@ Bookfields = {
 class Bookitems(Resource):
     @marshal_with(Bookfields)
     def get(self):
-        books = Book.query.all()
+        books = Books.query.all()
         return books
 
     @marshal_with(Bookfields)
     def post(self):
         data = request.json
-        book = Book(title=data['title'])
+        book = Books(title=data['title'], author=data['author'], date=data['date'],
+                     publisher=data['publisher'], isbn=data['isbn'])
         db.session.add(book)
         db.session.commit()
-        books = Book.query.all()
+        books = Books.query.all()
         return books
 
 
 class Bookitem(Resource):
     @marshal_with(Bookfields)
     def get(self, id):
-        book = Book.query.filter_by(id=id).first()
+        book = Books.query.get_or_404(id)
         return book
 
     @marshal_with(Bookfields)
     def put(self, id):
-        data = request.json
-        book = Book.query.filter_by(id=id).first()
-        book.name = data['name']
+        value = request.json
+        book = Books.query.get_or_404(id)
+        book.title = value['title']
+        book.author = value['author']
+        book.date = value['date']
+        book.publisher = value['publisher']
+        book.isbn = value['isbn']
         db.session.commit()
         return book
 
     @marshal_with(Bookfields)
     def delete(self, id):
-        book = Book.query.filter_by(id=id).first()
+        book = Books.query.get_or_404(id)
         db.session.delete(book)
         db.session.commit()
-        books = Book.query.all()
+        books = Books.query.all()
         return books
 
 
